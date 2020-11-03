8f3930ce78db8ececad40b37225ecddfb5f44b68
Dahyna Guzmán <dahguz@gmail.com>
2020-11-03 17:02:59 -0300

    Auto stash before checking out "HEAD"

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/speed-feedback'));
 
-app.get('/', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/speed-feedback/index.html'));
+app.get('/', (req,res)=> {
+    res.sendFile(path.join(__dirname+'/dist/speed-feedback/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080, ()=> {console.log('Server Listen');});
+app.listen(process.env.PORT || 8081, ()=> {console.log('Server Listen');});
